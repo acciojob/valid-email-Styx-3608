b8c37d66acbe7e149ced15a2214623211ca4cdb8
Saniya Ahmad <130426902+Styx-3608@users.noreply.github.com>
2025-06-18 10:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
-  //your JS code here.
+  // Return false for empty input
+  if (!str) return false;
+
+  // Regular expression to validate email
+  const regex = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,6})+$/;
+  return regex.test(str);
 }
 
 // Do not change the code below.
